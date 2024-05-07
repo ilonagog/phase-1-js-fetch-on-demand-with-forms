0ccb3e6b75729af12d1302d4dff3fbd72600da50
ilona <gogiashvili.ilona@gmail.com>
2024-05-07 12:08:10 -0400

    commit tree af86bb8345900ed203d977a2b1b9f722cbc980ba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  const formD = document.querySelector("form");
+  formD.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#searchByID");
+    console.log(input);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
